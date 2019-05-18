9991c78234bc4fb03c5dd731e56a6bd0bcc23000
Maayan <maayan.hadarwork@gmail.com>
2019-05-18 09:11:34 +0300

    a lot of changes - in both- combined with main

--- Project/function1.c
+++ Project/function1.c
@@ -1,34 +1,157 @@
 #include "PrototypesProject.h"
-static BOOL isCellFree(Board board, checkersPos *soldier);
+// Public  Prototypes
 void checkAllocation(void *address);
+
+// function1 Prototypes
+static void soldierStatus(Board board, checkersPos *src, player pl, checkersPos *options, checkersPos *captures);
+// return in the options array the nearest possible coordinates for moving and in accordance the place of an eaten soldier if exists
+SingleSourceMovesTree FindSingleSourceMovesAux(Board board, checkersPos *src, player pl); // Auxiliary function for building the tree moves
+
+static BOOL isCellFree(Board board, checkersPos *soldier);
 static BOOL isBottom(player p);
-checkersPos *soldierStatus(Board board, checkersPos *src, player pl);
-// blah blah
-checkersPos *soldierStatus(Board board, checkersPos *src, player pl) //Added option for player(inserting B OR T).
-{                                                                    //Rows can get input from 'A' to 'H', columns can get input from '1' to '8'.
-    checkersPos *options[2];
-    options[0] = options[1] = NULL;
-    if (pl == 'T')
-    {//hello
-        if ((src->col != '1') && (src->col != '8') && (src->row != 'H')) //might have steps from both sides.
-        {//Test 1:Checking if sides are empty.
-            checkersPos *solider;
-            solider->col = src->col - 1;
-            solider->row = src->row + 1;
-            if (isCellFree(board, solider))
-            { //Checking left side of 'T' step option.
-                options[0] = solider;
-            }
-            solider->col = src->col + 1;
-            if (isCellFree(board, solider))
-            { //Checking right side of 'T' step option.
-                options[1] = solider;
-            }
-        }//Unfinished.
-        if((src->col>=3)&&(src->col<=6))
+
+// Tests- Static Prototypes
+static void testsForT(Board board, checkersPos *src, checkersPos *options, checkersPos *captures);
+static void testsForB(Board board, checkersPos *src, checkersPos *options, checkersPos *captures);
+static void testsForTLeft(Board board, checkersPos *src, checkersPos *options, checkersPos *captures);
+static void testsForTRight(Board board, checkersPos *src, checkersPos *options, checkersPos *captures);
+static void testsForBLeft(Board board, checkersPos *src, checkersPos *options, checkersPos *captures);
+static void testsForBRight(Board board, checkersPos *src, checkersPos *options, checkersPos *captures);
+
+static void initializePlayer(Board board, checkersPos *src, player *currentPlayer); // Initializing the player based on the given coordinate
+static void initializeBoardOfPlayer(Board board, Board *boardOfPlayer);             // Initializing the board of a given coordinate
+static void handleBoardChange(Board board, checkersPos *src, player pl, checkersPos *options, checkersPos *captures);
+// Update the board based on possible near moves and captions
+static BOOL isThereACapture(checkersPos *options, checkersPos *captures, int *ind); // Determines whether there's an option of move with a
+                                                                                    // capture between the given captures, if exist
+                                                                                    // Return true in case there's a match between a capture and
+static void fillOptions(checkersPos *soldier, checkersPos *options);
+static void fillCaptures(checkersPos *soldier, checkersPos *captures);
+
+SingleSourceMovesTree *FindSingleSourceMoves(Board board, checkersPos *src)
+{
+    SingleSourceMovesTree *movesTree = NULL;
+    player currentPlayer;
+
+    if (!isCellFree(board, src)) // In case the cell is taken
+    {
+        initializePlayer(board, src, &currentPlayer);
+        *movesTree = FindSingleSourceMovesAux(board, src, currentPlayer);
+    }
+
+    return (movesTree); // Returns the tree moves or a NULL in case the cell is free
+}
+
+static void initializePlayer(Board board, checkersPos *src, player *currentPlayer)
+{
+    *(currentPlayer) = board[src->row - 'A'][src->col - '0' - 1]; // Initializing the player found in the given coordinate
+}
+
+SingleSourceMovesTree FindSingleSourceMovesAux(Board board, checkersPos *src, player pl)
+{
+    // Initialized due to security reasons
+    SingleSourceMovesTree treeOne = {NULL}, treeTwo = {NULL}, baseTree = {NULL};
+    checkersPos *options = NULL, *captures = NULL;
+
+    // Allocating a node
+    baseTree.source = (SingleSourceMovesTreeNode *)malloc(sizeof(SingleSourceMovesTreeNode));
+    checkAllocation(baseTree.source); // Checking whether the memory allocation succeeded or not
+                                      // To update the pointer to soldier
+    if (baseTree.source == NULL)
+    {
+        return baseTree;
+    }
+    else
+    { // Allocating 2 arrays for the near moves / captures
+        options = (checkersPos *)calloc(2, sizeof(checkersPos));
+        checkAllocation(options);
+        captures = (checkersPos *)calloc(2, sizeof(checkersPos));
+        checkAllocation(captures);
+
+        initializeBoardOfPlayer(board, &baseTree.source->board); // Initializing board of the current root
+        soldierStatus(board, src, pl, options, captures);        // Returns an array of 2 possible moves- index 0 to the left, index 1 to the right
+        handleBoardChange(board, src, pl, options, captures);   // Handles change due to next move update in the the tree
+        baseTree.source->pos = src;                              // Updating the position of the current node
+
+        treeOne = FindSingleSourceMovesAux(board, &options[0], pl);
+        treeTwo = FindSingleSourceMovesAux(board, &options[1], pl);
+
+        // Free the arrays allocated at start
+        free(options);
+        free(captures);
+
+        // Compliment case - Connecting sub - Trees to the base Tree
+        baseTree.source->next_move[0] = treeOne.source;
+        baseTree.source->next_move[1] = treeOne.source;
+        return(baseTree);
+    }
+}
+static void initializeBoardOfPlayer(Board board, Board *boardOfPlayer)
+{
+    int i = 0, j = 0;
+
+    for (i = 0; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
         {
-            
+            (*boardOfPlayer)[i][j] = board[i][j];
         }
+    }
+}
+
+static void handleBoardChange(Board board, checkersPos *src, player pl, checkersPos *options, checkersPos *captures)
+{
+    int Ind = -1; // Index for the array of captures, Initialized in case there's a matched capture
+
+    if (isThereACapture(options, captures, &Ind))
+    {
+        (board)[src->row - 'A'][src->col - '0' - 1] = ' ';                   // Removing the player from its last position
+        (board)[options[0].row - 'A'][options[0].col - '0' - 1] = pl;        // Moving the player to the place given in options array
+        (board)[captures[Ind].row - 'A'][captures[Ind].col - '0' - 1] = ' '; // Removing the captured player from board
+    }
+    else // No capture - updating the table by moving the player and deleting its last location
+    {
+        (board)[src->row - 'A'][src->col - '0' - 1] = ' '; // Removing the player from its last position
+        (board)[options[0].row - 'A'][options[0].col - '0' - 1] = pl;
+    }
+}
+
+static BOOL isThereACapture(checkersPos *options, checkersPos *captures, int *ind)
+{
+    // Check for the first potential capture                                                                                // Checks wether one of the captions matches the move option
+    if ((captures[0].col != 0) && (captures[0].row != 0))
+    {                                                                                                 // In case its a valid coordinate
+        if (abs(captures[0].col - options[0].col) == 1 && abs(captures[0].col - options[0].col) == 1) // The distance between the column coordinate and the row's coordinate is one- (diagonal- marks a capture)
+        {
+            *ind = 0; // Updating the index of the capture
+            return TRUE;
+        }
+    }
+    // Check for the second potential capture
+    else if ((captures[1].col != 0) && (captures[1].row != 0))
+    {
+        if (abs(captures[1].col - options[1].col) == 1 && abs(captures[1].col - options[1].col) == 1) // The distance between the column coordinate and the row's coordinate is one- (diagonal- marks a capture)
+        {
+            *ind = 1;
+            return TRUE;
+        }
+    }
+    else // In any other case
+        return FALSE;
+}
+
+static void soldierStatus(Board board, checkersPos *src, player pl, checkersPos *options, checkersPos *captures) //Added option for player(inserting B OR T).
+{                                                                                                                //Rows can get input from 'A' to 'H', columns can get input from '1' to '8'.
+                                                                                                                 // you can't initialize a struct type with NULL, that's why
+                                                                                                                 // when you added the extra asterisk it worked- because the elements
+                                                                                                                 // in the array became pointers, and than its legit to initialize with NULL
+    if (pl == 'T')                                                                                               // In case 'T' is the player
+    {
+        testsForT(board, src, options, captures);
+    }
+    else // In case 'B' is the player
+    {
+        testsForB(board, src, options, captures);
     }
 }
 void checkAllocation(void *address)
@@ -40,66 +163,193 @@ void checkAllocation(void *address)
     }
 }
 
-SingleSourceMovesTree *FindSingleSourceMoves(Board board, checkersPos *src)
-{
-    SingleSourceMovesTree *movesTree;
-
-    if (isCellFree(board, src))
-    {
-        *movesTree = FindSingleSourceMovesAux(board, src);
-    }
-    else
-    {
-        movesTree = NULL;
-
-        return movesTree;
-    }
-}
-
-SingleSourceMovesTree FindSingleSourceMovesAux(Board board, checkersPos *src) // Aux
-{
-    SingleSourceMovesTree treeOne, treeTwo, baseTree = {0};
-    checkersPos *arr;
-
-    // Allocating a node
-    baseTree.source = (SingleSourceMovesTreeNode *)malloc(sizeof(SingleSourceMovesTreeNode));
-    checkAllocation(baseTree.source); // Checking whether the memory allocation succeeded or not
-                                      // To update the pointer to soldier
-    if (baseTree.source == NULL)
-    {
-        return baseTree;
-    }
-    else
-    {
-        if (isBottom)
-        {
-            arr = (checkersPos*)malloc(2 * sizeof(checkersPos));
-            arr = soldierStatus(board, src, 'B'); // Returns an array of 2 possible moves- index 0 to the left, index 1 to the right
-            treeOne = FindSingleSourceMovesAux(board, src);
-            treeTwo = FindSingleSourceMovesAux(board, src);
-        }
-        else
-        {
-            treeOne = FindSingleSourceMovesAux(board, src);
-            treeTwo = FindSingleSourceMovesAux(board, src);
-        }
-    }
-}
-
 static BOOL isBottom(player p)
 {
     if (p == 'B')
         return TRUE;
-    else 
+    else
         return FALSE;
 }
 
 static BOOL isCellFree(Board board, checkersPos *soldier)
-{
-    if (board[soldier->col][soldier->row] != ' ') // Space marks empty cell
+{                                                                           // converting the characters from an ASCII values to decimal numbers (indexes)
+    if (board[(int)soldier->row - 'A'][(int)soldier->col - '0' - 1] != ' ') // Space marks empty cell
     {
         return TRUE;
     }
     else
         return FALSE;
+}
+
+//**********************************files separation - tests********************************************//
+
+static void testsForTLeft(Board board, checkersPos *src, checkersPos *options, checkersPos *captures)
+{
+    checkersPos *soldier;
+    soldier = (checkersPos *)malloc(sizeof(checkersPos));
+    checkAllocation(soldier);
+    if ((src->col != '1') && (src->col != '8') && (src->row != 'H')) //might have steps from both sides.
+    {                                                                //Test 1:Checking if sides are empty.
+        soldier->col = src->col - 1;
+        soldier->row = src->row + 1;
+        if (isCellFree(board, soldier))
+        { //Checking left side of 'T' step option.
+            fillOptions(soldier, options);
+        }
+        else if ((src->col >= '3') && (src->col <= '6') && (src->row <= 'F'))
+        { //TEST 2: checks if the soldier can eat an enemy soldier.
+            soldier = src;
+            soldier->row = soldier->row + 1;
+            soldier->col = soldier->col - 1;
+            if ((!isCellFree(board, soldier) && (board[(int)(soldier->row) - 'A'][soldier->col - '0' - 1] == 'B')))
+            { //Checks if the variable of the left is an enemy.
+                fillCaptures(soldier, captures);
+                soldier->row = soldier->row + 1;
+                soldier->col = soldier->col - 1;
+                if (isCellFree(board, soldier))
+                { //if the next step on the right is free jump over the enemy("eat it").
+                    fillOptions(soldier, options);
+                }
+            }
+        }
+    }
+}
+static void testsForTRight(Board board, checkersPos *src, checkersPos *options, checkersPos *captures)
+{
+    checkersPos *soldier;
+    soldier = (checkersPos *)malloc(sizeof(checkersPos));
+    checkAllocation(soldier);
+
+    if ((src->col != '1') && (src->col != '8') && (src->row != 'H')) //might have steps from both sides.
+    {                                                                //Test 1:Checking if sides are empty.
+        soldier->row = src->row + 1;
+        soldier->col = src->col + 1;
+        if (isCellFree(board, soldier))
+        { //Checking right side of 'T' step option.
+            fillOptions(soldier, options);
+        }
+        else if ((src->col >= '3') && (src->col <= '6') && (src->row <= 'F'))
+        { //TEST 2: checks if the soldier can eat an enemy soldier.
+            soldier = src;
+            soldier->row = src->row + 1;
+            soldier->col = src->col + 1;
+            if ((!isCellFree(board, soldier) && (board[(int)soldier->row - 'A'][soldier->col - '0' - 1] == 'B')))
+            { //Checks if the variable of the right is an enemy.
+                fillCaptures(soldier, options);
+                soldier->row = soldier->row + 1;
+                soldier->col = soldier->col + 1;
+                if (isCellFree(board, soldier))
+                {
+                    fillOptions(soldier, options);
+                }
+            }
+        }
+    }
+}
+static void testsForB(Board board, checkersPos *src, checkersPos *options, checkersPos *captures)
+{
+    testsForBLeft(board, src, options, captures);
+    testsForBRight(board, src, options, captures);
+}
+static void testsForT(Board board, checkersPos *src, checkersPos *options, checkersPos *captures)
+{
+    testsForTLeft(board, src, options, captures);
+    testsForTRight(board, src, options, captures);
+}
+
+static void testsForBLeft(Board board, checkersPos *src, checkersPos *options, checkersPos *captures)
+{
+    checkersPos *soldier;
+    soldier = (checkersPos *)malloc(sizeof(checkersPos));
+    checkAllocation(soldier);
+
+    if ((src->col != '1') && (src->col != '8') && (src->row != 'A')) //might have steps from both sides.
+    {                                                                //Test 1:Checking if sides are empty.
+        soldier->col = src->col - 1;
+        soldier->row = src->row - 1;
+        if (isCellFree(board, soldier))
+        { //Checking left side of 'T' step option.
+            fillOptions(soldier, options);
+        }
+
+        else if ((src->col >= '3') && (src->col <= '6') && (src->row <= 'C'))
+        { //TEST 2: checks if the soldier can eat an enemy soldier.
+            soldier = src;
+            soldier->row = soldier->row - 1;
+            soldier->col = soldier->col - 1;
+            if ((!isCellFree(board, soldier) && (board[(int)(soldier->row - 'A')][soldier->col - '0' - 1] == 'T')))
+            { //Checks if the variable of the left is an enemy.
+                fillCaptures(soldier, captures);
+                soldier->row = soldier->row - 1;
+                soldier->col = soldier->col - 1;
+                if (isCellFree(board, soldier))
+                { //if the next step on the right is free jump over the enemy("eat it").
+                    fillOptions(soldier, options);
+                }
+            }
+        }
+    }
+}
+static void testsForBRight(Board board, checkersPos *src, checkersPos *options, checkersPos *captures)
+{
+
+    checkersPos *soldier;
+    soldier = (checkersPos *)malloc(sizeof(checkersPos));
+    checkAllocation(soldier);
+
+    if ((src->col != '1') && (src->col != '8') && (src->row != 'A')) //might have steps from both sides.
+    {                                                                //Test 1:Checking if sides are empty.
+        soldier->col = src->col + 1;
+        soldier->row = src->row - 1;
+        if (isCellFree(board, soldier))
+        { //Checking left side of 'T' step option.
+            fillOptions(soldier, options);
+        }
+
+        else if ((src->col >= '3') && (src->col <= '6') && (src->row <= 'C'))
+        { //TEST 2: checks if the soldier can eat an enemy soldier.
+            soldier = src;
+            soldier->row = soldier->row - 1;
+            soldier->col = soldier->col + 1;
+            if ((!isCellFree(board, soldier) && (board[(int)soldier->row - 'A'][soldier->col - '0' - 1] == 'T')))
+            { //Checks if the variable of the left is an enemy.
+                fillCaptures(soldier, captures);
+                soldier->row = soldier->row - 1;
+                soldier->col = soldier->col + 1;
+                if (isCellFree(board, soldier))
+                { //if the next step on the right is free jump over the enemy("eat it").
+                    fillOptions(soldier, options);
+                }
+            }
+        }
+    }
+}
+
+static void fillOptions(checkersPos *soldier, checkersPos *options)
+{                                                       // Remember to initialize options array and captures array with '0' in each cell of the arrays
+    if (options[0].col == '0' && options[0].row == '0') // first cell is empty
+    {
+        options[0].col = soldier->col;
+        options[0].row = soldier->row;
+    }
+    else if (options[1].col == '0' && options[1].row == '0') // second cell is empty
+    {
+        options[1].col = soldier->col;
+        options[1].row = soldier->row;
+    }
+    return; // in case both cells are taken
+}
+static void fillCaptures(checkersPos *soldier, checkersPos *captures)
+{                                                         // Remember to initialize options array and captures array with '0' in each cell of the arrays
+    if (captures[0].col == '0' && captures[0].row == '0') // first cell is empty
+    {
+        captures[0].col = soldier->col;
+        captures[0].row = soldier->row;
+    }
+    else if (captures[1].col == '0' && captures[1].row == '0') // second cell is empty
+    {
+        captures[1].col = captures->col;
+        captures[1].row = captures->row;
+    }
+    return; // in case both cells are taken
 }
---
+++ Project/main.c
@@ -0,0 +1,130 @@
+#include "PrototypesProject.h"
+
+static void PrintOpener();
+void fillStartingBoard(Board *board);
+static void printBoard(Board board);
+static void printTreeInOrder  (SingleSourceMovesTree* movesTree1);
+ 
+int main()
+{
+    // Opener- Checkers game
+    //PrintOpener();
+    Board startingBoard;
+    // Fill board
+    fillStartingBoard(&startingBoard);
+    printBoard(startingBoard);
+
+    // for end cases
+    Board testBoard={
+                {' ',' ',' ',' ',' ',' ',' ',' '},
+                {' ',' ','T',' ',' ',' ',' ',' ',},
+                {' ',' ',' ',' ',' ',' ',' ',' '},
+                {' ',' ',' ',' ',' ',' ',' ',' '},
+                {' ',' ',' ',' ',' ',' ',' ',' '},
+                {'B',' ',' ',' ',' ',' ',' ',' '},
+                {' ',' ',' ',' ',' ',' ',' ',' '},
+                {' ',' ',' ',' ',' ',' ',' ',' '},
+    };
+    printBoard(testBoard);
+
+checkersPos src1, src2; 
+// test1-top-middle board
+src1.row=1;src1.row=2;
+// test2- bottom-middleside
+src2.row=1;src2.row=2;
+
+printf ("src1 coordinate: %c X %c \n , src2 coordinate: %c X %c\n", src1.row, src1.col, src2.row, src2.col);
+
+SingleSourceMovesTree *movesTree1= FindSingleSourceMoves(startingBoard, &src1);
+SingleSourceMovesTree *movesTree2= FindSingleSourceMoves(startingBoard, &src2);
+
+SingleSourceMovesTree *movesTree3= FindSingleSourceMoves(testBoard, &src2);
+SingleSourceMovesTree *movesTree4= FindSingleSourceMoves(testBoard, &src2);
+
+void printTreeInOrder  (SingleSourceMovesTree* movesTree);
+static void printTreeInOrderAux  (SingleSourceMovesTreeNode* source );
+
+printTreeInOrder (movesTree1);
+printTreeInOrder (movesTree2);
+printTreeInOrder (movesTree3);
+printTreeInOrder (movesTree4);
+
+    return 0;
+}
+
+static void printBoard(Board board) // debugging purpose only 
+{
+    int i, j;
+    for (i=0;i<8;i++)
+    {
+        for(j=0;j<8;j++)
+        {
+            printf(" %c ", board[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+void printTreeInOrder  (SingleSourceMovesTree* movesTree)
+{
+    if (movesTree==NULL)
+        return;
+    else
+        printTreeInOrderAux(movesTree->source);
+}
+static void printTreeInOrderAux  (SingleSourceMovesTreeNode* source )
+{
+    if (source==NULL)
+        return;
+    else
+    {
+        printTreeInOrderAux(source->next_move[0]);
+        printBoard(source->board);
+        printTreeInOrderAux(source->next_move[1]);
+    }
+}
+
+
+
+void fillStartingBoard(Board *board)
+{
+    int i = 0, j = 0;
+
+    // Filling top -A TO C
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            if( ((i % 2 == 0) && (j % 2 == 1)) || ((i % 2 == 0) && (j % 2 == 1)) )
+                (*board)[i][j] = 'T'; // Top player
+            else
+                (*board)[i][j] = ' ';
+        }
+    }
+    // Filling middle- 2 empty rows - D , E
+    for (i = 0; j < 2; j++)
+        for (j = 0; j < 8; j++)
+            (*board)[i][j] = ' ';
+    // Filling bottom - F TO H
+    for (i = 5; i < 8; i++)
+    {
+        for (j = 0; j < 8; j++)
+        {
+            if( ((i % 2 == 0) && (j % 2 == 1)) || ((i % 2 == 0) && (j % 2 == 1)) )
+                (*board)[i][j] = 'B'; // Bottom player
+            else
+                (*board)[i][j] = ' ';
+        }
+    }
+}
+static void PrintOpener()
+{
+    printf(":'######::'##::::'##:'########::'######::'##:::'##:'########:'########:::'######::\n");
+    printf("'##... ##: ##:::: ##: ##.....::'##... ##: ##::'##:: ##.....:: ##.... ##:'##... ##:\n");
+    printf("##:::..:: ##:::: ##: ##::::::: ##:::..:: ##:'##::: ##::::::: ##:::: ##: ##:::..::\n");
+    printf("##::::::: #########: ######::: ##::::::: #####:::: ######::: ########::. ######::\n");
+    printf(" ##::::::: ##.... ##: ##...:::: ##::::::: ##. ##::: ##...:::: ##.. ##::::..... ##:\n");
+    printf("##::: ##: ##:::: ##: ##::::::: ##::: ##: ##:. ##:: ##::::::: ##::. ##::'##::: ##:\n");
+    printf(". ######:: ##:::: ##: ########:. ######:: ##::. ##: ########: ##:::. ##:. ######::\n");
+    printf(":......:::..:::::..::........:::......:::..::::..::........::..:::::..:::......:::\n");
+}
