1e4f2aa8e850d034cfcfe426ee0ff9bc7f8221c1
Maayan <maayan.hadarwork@gmail.com>
2019-06-09 16:54:54 +0300

    All fixed- changed the make empty list to return the pointer instead of void, function 4 is readable and looks nice- combined

---
+++ Project/Build_List.c
@@ -0,0 +1,88 @@
+
+#include "PrototypesProject.h"
+static void FindSingleSourceOptimalMoveAux(SingleSourceMovesTreeNode *source, SingleSourceMovesList **OptimalMoveList);
+static void insertBestMove(SingleSourceMovesList **OptimalMoveList, SingleSourceMovesList **leftList, SingleSourceMovesList **rightList,
+	SingleSourceMovesTreeNode *source);
+
+SingleSourceMovesList *FindSingleSourceOptimalMove(SingleSourceMovesTree *movesTree)
+{
+	SingleSourceMovesList *OptimalMoveList = makeEmpty_List(); // Defined as pointer to avoid return of a local variable
+	// Making an empty list 
+	// Count Captures of returned list.
+	if (movesTree == NULL)					 // Empty tree case - design by contract- not suppose to happen
+	{
+		return NULL;
+	}
+	else
+	{
+		FindSingleSourceOptimalMoveAux(movesTree->source, &OptimalMoveList);		// Finds the single source optimal moves
+		OptimalMoveList->head->captures = movesTree->source->total_captures_so_far; //Updating head captures.
+	}
+	return (OptimalMoveList); // Returns the List represents the single source optimal moves
+}
+static void FindSingleSourceOptimalMoveAux(SingleSourceMovesTreeNode *source, SingleSourceMovesList **OptimalMoveList)
+{
+	SingleSourceMovesList *leftList = makeEmpty_List(), *rightList = makeEmpty_List();
+	SingleSourceMovesListCell *node;
+
+	 // Making empty lists for the sub trees
+
+	if ((source->next_move[0] == NULL) && (source->next_move[1] == NULL)) // A leaf - a single move tree- it is the optimal
+	{																	  //Inserting the new Leaf into the list.
+		node = createNode(source->pos, (*OptimalMoveList)->head);
+		(*OptimalMoveList)->head = (*OptimalMoveList)->tail = node;
+		(*OptimalMoveList)->head->captures = source->total_captures_so_far; // Updating captures of the new node
+	}
+	else
+	{
+		//	 Recursive calls
+		if (source->next_move[0] != NULL)
+		{
+			FindSingleSourceOptimalMoveAux(source->next_move[0], &leftList);
+			leftList->head->captures = source->next_move[0]->total_captures_so_far; // Updating captures of the new node
+		}
+		if (source->next_move[1] != NULL)
+		{
+			FindSingleSourceOptimalMoveAux(source->next_move[1], &rightList);
+			rightList->head->captures = source->next_move[1]->total_captures_so_far; // Updating captures of the new node
+		}
+		insertBestMove(OptimalMoveList, &leftList, &rightList, source);
+		// Check which move is better from the 'returned' options of sub tree right and sub tree right
+	}
+}
+
+
+
+static void insertBestMove(SingleSourceMovesList **OptimalMoveList, SingleSourceMovesList **leftList, SingleSourceMovesList **rightList,
+	SingleSourceMovesTreeNode *source)
+{//Check which moves are valid and insert the best move possible.
+	SingleSourceMovesListCell *node; // Initialized due to security reasons
+
+	if ((*leftList)->head == NULL)
+	{ //There is no left List.
+		node = createNode(source->pos, (*rightList)->head);
+		(*rightList)->head = node;
+		insertListToHead(OptimalMoveList, *rightList); // Updating List from start
+													   //(*OptimalMoveList)->head->captures = rightList->tail->captures; // Updating captures of the new node
+	}
+	else if ((*rightList)->head == NULL)
+	{ //There is no right List.
+		node = createNode(source->pos, ((*leftList)->head));
+		(*leftList)->head = node;
+		insertListToHead(OptimalMoveList, (*leftList)); // Updating List from start
+	}
+	else if ((*leftList)->tail->captures > (*rightList)->tail->captures) // Left move is better
+	{
+		node = createNode(source->pos, ((*leftList)->head));
+		(*leftList)->head = node;
+		insertListToHead(OptimalMoveList, (*leftList)); // Updating List from start
+		freeList(*rightList);//No use for rightList anymore.
+	}
+	else // Right move is better
+	{
+		node = createNode(source->pos, (*rightList)->head);
+		(*rightList)->head = node;
+		insertListToHead(OptimalMoveList, (*rightList)); // Updating List from start.
+		freeList(*leftList);//No use for leftList anymore.
+	}
+}
--- Project/PrototypesProject.h
+++ Project/PrototypesProject.h
@@ -10,55 +10,57 @@
 #define MAX(a, b) (a > b ? a : b)
 
 typedef int BOOL;
+
 /*Structs*/
+
 typedef struct __checkersPos
 {
-    char row, col;
+	char row, col;
 } checkersPos;
-/*Structs*/
 typedef unsigned char Board[BOARD_SIZE][BOARD_SIZE];
 typedef unsigned char player;
 typedef struct __SingleSourceMovesTreeNode
 {
-    Board board;
-    checkersPos *pos;
-    unsigned short total_captures_so_far;            //Number of Captures
-    struct __SingleSourceMovesTreeNode *next_move[2]; // movement destinations
+	Board board;
+	checkersPos *pos;
+	unsigned short total_captures_so_far;             //Number of Captures
+	struct __SingleSourceMovesTreeNode *next_move[2]; // movement destinations
 } SingleSourceMovesTreeNode;
 
 typedef struct __SingleSourceMovesTree
 {
-    SingleSourceMovesTreeNode *source;
+	SingleSourceMovesTreeNode *source;
 } SingleSourceMovesTree;
 
 typedef struct __SingleSourceMovesList
 {
-    struct __SingleSourceMovesListCell *head;
-    struct __SingleSourceMovesListCell *tail;
+	struct __SingleSourceMovesListCell *head;
+	struct __SingleSourceMovesListCell *tail;
 } SingleSourceMovesList;
 typedef struct __SingleSourceMovesListCell
 {
-    checkersPos *position;                    // Indicates the position of player
-    unsigned short captures;                  // number of captures per position
-    struct __SingleSourceMovesListCell *next; // next move to be done
+	checkersPos *position;                    // Indicates the position of player
+	unsigned short captures;                  // number of captures per position
+	struct __SingleSourceMovesListCell *next; // next move to be done
 } SingleSourceMovesListCell;
 
 typedef struct __MultipleSourceMovesListCell
 {
-    struct __SingleSourceMovesList *Single_Source_moves_list; // Pointer to a single source moves list
-    struct __MultipleSourceMovesListCell *next;               // Pointer to the next list
+	struct __SingleSourceMovesList *Single_Source_moves_list; // Pointer to a single source moves list
+	struct __MultipleSourceMovesListCell *next;               // Pointer to the next list
 } MultipleSourceMovesListCell;
 
 typedef struct __MultipleSingleSourceMovesList
 {
-    struct __MultipleSourceMovesListCell *head; // Points to the first list in the multiple-source-moves list
-    struct __MultipleSourceMovesListCell *tail; // Points to the last list in the multiple-source-moves list
+	struct __MultipleSourceMovesListCell *head; // Points to the first list in the multiple-source-moves list
+	struct __MultipleSourceMovesListCell *tail; // Points to the last list in the multiple-source-moves list
 } MultipleSingleSourceMovesList;
+/*Structs*/
 
 /*Declartions*/
 SingleSourceMovesTree *FindSingleSourceMoves(Board board, checkersPos *src);
 void checkAllocation(void *address);
-void handleBoardChange(Board board, checkersPos *src, player pl, int direction, checkersPos *options, checkersPos *captures, unsigned int *countCaptures);
+void handleBoardChange(Board board, checkersPos *src, player pl, int direction, checkersPos *options, checkersPos *captures, unsigned short *countCaptures);
 void fillStartingBoard(Board *board);
 
 //Fill
@@ -67,32 +69,45 @@ void fillCaptures(checkersPos *soldier, checkersPos *captures, int direction);
 //Fill
 
 /*Tests*/
+BOOL isThereACapture(checkersPos *options, checkersPos *captures, int *ind); // Determines whether there's an option of a move with a
 void soldierStatus(Board board, checkersPos *src, player pl, checkersPos *options, checkersPos *captures); //Added option for player(inserting B OR T).
 BOOL isCellFree(Board board, checkersPos *soldier);
 /*Tests*/
 
 /*Printing*/
+void PrintOpener();
+void fillStartingBoard(Board *board);
 void printTreeInOrder(SingleSourceMovesTree *movesTree);
 void printBoard(Board board);
 void printTreeBoardInOrderAux(SingleSourceMovesTreeNode *source);
-
 void printTreeInOrderAux(SingleSourceMovesTreeNode *source);
- void printTreeSourcesInOrderAux(SingleSourceMovesTreeNode *source);
+void printTreeSourcesInOrderAux(SingleSourceMovesTreeNode *source);
 
 // void printTreeSourcesInOrderAux(SingleSourceMovesTreeNode *source);
 void PrintOpener();
 
 /*Printing*/
 
-/*Declartions*/
+/*Tree Implementations*/
+void initializePlayer(Board board, checkersPos *src, player *currentPlayer);											   // Initializing the player based on the given coordinate
+void initializeBoardOfPlayer(Board board, player pl, Board *boardOfPlayer, checkersPos src, checkersPos *options, checkersPos *captures);
+void allocateTreeNode(SingleSourceMovesTree *baseTree, checkersPos *src);	   // Allocates tree Node
+void allocateOptionsAndCaptures(checkersPos **options, checkersPos **captures); //Allocate options and captures arrays.
+void makeLeaf(SingleSourceMovesTreeNode *leaf, checkersPos *src);			   //Makes a leaf.
+void handleLeaf(SingleSourceMovesTree *baseTree, Board board, player pl, checkersPos *src, checkersPos *options, checkersPos *captures);
+void freeTree(SingleSourceMovesTree* tr); // Frees allocated memory for the tree
+/*Tree Implementations*/
 
+
+//List Implementations.
 void freeList(SingleSourceMovesList *lst);
 SingleSourceMovesListCell *createNode(checkersPos *pos, SingleSourceMovesListCell *next); // Creates a node in the list
-void insertNodeToTail(SingleSourceMovesList *lst, SingleSourceMovesListCell *newTail);    // Adding the new to the tail of the list
-void insertNodeToHead(SingleSourceMovesList *lst, SingleSourceMovesListCell *newHead);    // Adding an element to the start of the list
-BOOL isEmptyList(SingleSourceMovesList *lst);                                             // Determines whether the list is empty or not
-void makeEmptyList(SingleSourceMovesList *lst);                                           // Makes empty list
-void printList(SingleSourceMovesList *lst);                                               // Prints the list of single source move
+void insertNodeToTail(SingleSourceMovesList *lst, SingleSourceMovesListCell *newTail);   // Adding the new to the tail of the list
+void insertListToHead(SingleSourceMovesList **lst, SingleSourceMovesList*newHead);		// Adding an element to the start of the list
+BOOL isEmptyList(SingleSourceMovesList *lst);                                            // Determines whether the list is empty or not
+SingleSourceMovesList* makeEmpty_List();												// Makes empty list
+void printList(SingleSourceMovesList *lst);                                            // Prints the list of single source move
+//List Implementations.
 
 // Function 2
 SingleSourceMovesList *FindSingleSourceOptimalMove(SingleSourceMovesTree *movesTree);
@@ -102,8 +117,12 @@ MultipleSingleSourceMovesList *FindAllPossiblePlayerMoves(Board board, player pl
 
 void freeLoList(MultipleSingleSourceMovesList *Lol);
 MultipleSourceMovesListCell *createLolNode(SingleSourceMovesList *singleSourceMovesList, MultipleSourceMovesListCell *next); // Creates a node of List of lists
-void insertLolNodeToTail(MultipleSingleSourceMovesList *Lol, MultipleSourceMovesListCell *newTail);                          // Adding the new to the tail (last list) of the list
+void insertLolNodeToTail(MultipleSingleSourceMovesList *Lol, MultipleSourceMovesListCell *newTail);                         // Adding the new to the tail (last list) of the list
 //void insertLolNodeToHead(MultipleSingleSourceMovesList* Lol , MultipleSourceMovesListCell *newHead); // Adding the list to the start of the list of lists
 BOOL isEmptyLoList(MultipleSingleSourceMovesList *Lol);   // Determines whether the list is empty or not
-void makeEmptyLoList(MultipleSingleSourceMovesList *Lol); // Makes empty list
+MultipleSingleSourceMovesList * makeEmpty_LoList(); // Makes empty list - allocates a node
 void printLoList(MultipleSingleSourceMovesList *Lol);     // Prints the list of single source move
+/*Declartions*/
+
+// Function 4 
+void Turn(Board board, player player);
